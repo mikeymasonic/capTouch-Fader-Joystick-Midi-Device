a814334b31a82dd50991b4a77bae706e35ab291f
Michael Romay <amerror@gmail.com>
2020-01-20 16:46:52 -0800

    adjusted pins

--- capTouch-Fader-Joystick-Midi-Device.java
+++ capTouch-Fader-Joystick-Midi-Device.java
@@ -14,23 +14,23 @@ int faderPin3 = A18;
 int faderPin4 = A17;
 
 int joyPin1 = A16;
-int joyPin2 = A15;
+int joyPin2 = A13;
 
-int click1Pin = 5;
-int click2Pin = 4;
-int click3Pin = 3;
-int click4Pin = 2;
-int click5Pin = 13;
-
-//for capacitive touch buttons
-uint8_t touchPin[9] = {23, 22, 17, 16, 15, 33, 32, 0, 1};
-
-//create stored values for cap touch
-uint8_t ntouch = 9;
-boolean touchState[9] = {false, false, false, false, false, false, false, false, false};
-int touchValue = 0;
-uint8_t midiNotes[9] = {58, 59, 60, 62, 64, 65, 67, 69, 71}; // C oct5 major scale
-elapsedMillis msec = 0;
+int clickPin1 = 24;
+int clickPin2 = 10;
+int clickPin3 = 7;
+int clickPin4 = 4;
+int clickPin5 = 26;
+//
+////for capacitive touch buttons
+//uint8_t touchPin[9] = {23, 22, 17, 16, 15, 33, 32, 0, 1};
+//
+////create stored values for cap touch
+//uint8_t ntouch = 9;
+//boolean touchState[9] = {false, false, false, false, false, false, false, false, false};
+//int touchValue = 0;
+//uint8_t midiNotes[9] = {58, 59, 60, 62, 64, 65, 67, 69, 71}; // C oct5 major scale
+//elapsedMillis msec = 0;
 
 // declare your components here
 MIDIEncoder *enc1;
@@ -46,7 +46,7 @@ Potentiometer pot3(potPin3, midiChannel, 19, secondary, debug);
 Potentiometer fader1(faderPin1, midiChannel, 20, secondary, debug);
 Potentiometer fader2(faderPin2, midiChannel, 21, secondary, debug);
 Potentiometer fader3(faderPin3, midiChannel, 22, secondary, debug);
-Potentiometer fader4(faderPin3, midiChannel, 23, secondary, debug);
+Potentiometer fader4(faderPin4, midiChannel, 23, secondary, debug);
 
 Potentiometer joy1(joyPin1, midiChannel, 24, secondary, debug);
 Potentiometer joy2(joyPin2, midiChannel, 25, secondary, debug);
@@ -60,13 +60,13 @@ Button clk5(clickPin5, midiChannel, 30, secondary, debug);
 void setup() {
   
   Serial.begin(38400);
-  delay(200);
+//  delay(200);
 
-  enc1 = new MIDIEncoder(25,24,midiChannel,34,true, false);
-  enc2 = new MIDIEncoder(12,11,midiChannel,35,true, false);
-  enc3 = new MIDIEncoder(10,9,midiChannel,36,true, false);
-  enc4 = new MIDIEncoder(8,7,midiChannel,37,true, false);
-  enc5 = new MIDIEncoder(6,5,midiChannel,38,true, false);
+  enc1 = new MIDIEncoder(25,12,midiChannel,34,true, false);
+  enc2 = new MIDIEncoder(11,9,midiChannel,35,true, false);
+  enc3 = new MIDIEncoder(8,6,midiChannel,36,true, false);
+  enc4 = new MIDIEncoder(5,3,midiChannel,37,true, false);
+  enc5 = new MIDIEncoder(2,13,midiChannel,38,true, false);
 
   usbMIDI.setHandleNoteOff(OnNoteOff); //set event handler for note off
   usbMIDI.setHandleNoteOn(OnNoteOn); //set event handler for note on
@@ -74,7 +74,7 @@ void setup() {
 }
 
 void loop() {
-  //delay(10);
+  delay(10);
   pot1.read();
   pot2.read();
   pot3.read();
@@ -99,17 +99,17 @@ void loop() {
   clk4.read();
   clk5.read();
 
-// Scan the Capacitive Touch pins
-
-  for(uint8_t i=0; i < ntouch; i++) {   touchValue = touchRead(touchPin[i]);  // Serial.print("touchPin: ");  // Serial.print(touchPin[i]);  // Serial.print(" ");  // Serial.print("Value: ");  // Serial.println(touchValue);   if(touchValue > 2400 && !touchState[i]) {
-    usbMIDI.sendNoteOn(midiNotes[i], 99, 1);
-    touchState[i] = 1;
-  }
-  else if(touchValue < 2400 && touchState[i]) { usbMIDI.sendNoteOff(midiNotes[i], 0, 1); touchState[i] = 0; } 
-  }   
-  // Below is some code for adding a pot and button as MIDI CCs   if(msec >= 20) {
-    msec = 0;
-  }
+//// Scan the Capacitive Touch pins
+//
+//  for(uint8_t i=0; i < ntouch; i++) {
+//      touchValue = touchRead(touchPin[i]);  // Serial.print("touchPin: ");  // Serial.print(touchPin[i]);  // Serial.print(" ");  // Serial.print("Value: ");  // Serial.println(touchValue);   if(touchValue > 2400 && !touchState[i]) {
+//    usbMIDI.sendNoteOn(midiNotes[i], 99, 1);
+//    touchState[i] = 1;
+//  } else if (touchValue < 2400 && touchState[i]) { usbMIDI.sendNoteOff(midiNotes[i], 0, 1); touchState[i] = 0; }  
+//  // Below is some code for adding a pot and button as MIDI CCs   
+//  if(msec >= 20) {
+//    msec = 0;
+//  }
 
 //  while (usbMIDI.read()) {
 //
